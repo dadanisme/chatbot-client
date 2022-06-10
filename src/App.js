26cdd9a3b42199f68f986002eba60bac5aa15f2c
dadanisme <dadanisme@upi.edu>
2022-06-10 20:48:51 +0700

    add maps library

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "boxicons";
 import Chatbot from "./Components/Chatbot/Chatbot";
-
+import Maps from "./Components/Maps/Maps";
 function App() {
   return (
     <div className="App">
       <Chatbot />
+      <Maps />
     </div>
   );
 }
--- src/Components/Maps/Maps.js
+++ src/Components/Maps/Maps.js
@@ -2,10 +2,59 @@
 
 // function Maps() {
 //   const mapRef = useRef(null);
-//   const { lat, lng } = { lat: -34.397, lng: 150.644 };
-//   const position = google.maps.LatLng(lat, lng);
+//   var service;
+//   var infowindow;
 
-//   return <div ref={mapRef} style={{ width: "100%", height: "100%" }} />;
+//   useEffect(() => {
+//     const script = document.createElement("script");
+//     script.src =
+//       "https://maps.googleapis.com/maps/api/js?key=AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY&libraries=places&callback=initMap";
+//     script.defer = true;
+//     document.body.appendChild(script);
+
+//     window.initMap = function () {
+//       var map = new window.google.maps.Map(mapRef.current, {
+//         center: { lat: -34.397, lng: 150.644 },
+//         zoom: 8,
+//       });
+//       var infoWindow = new window.google.maps.InfoWindow();
+//       var service = new window.google.maps.places.PlacesService(map);
+//       service.nearbySearch(
+//         {
+//           location: { lat: -34.397, lng: 150.644 },
+//           radius: 500,
+//           type: ["restaurant"],
+//         },
+//         callback
+//       );
+//       function callback(results, status) {
+//         if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+//           for (var i = 0; i < results.length; i++) {
+//             createMarker(results[i]);
+//           }
+//         }
+//       }
+//       function createMarker(place) {
+//         var placeLoc = place.geometry.location;
+//         var marker = new window.google.maps.Marker({
+//           map: map,
+//           position: place.geometry.location,
+//         });
+//         window.google.maps.event.addListener(marker, "click", function () {
+//           infoWindow.setContent(place.name);
+//           infoWindow.open(map, this);
+//         });
+//       }
+//     };
+//   }, []);
+
+//   return (
+//     <div>
+//       <div ref={mapRef} style={{ width: "100%", height: "100%" }}>
+//         Hello
+//       </div>
+//     </div>
+//   );
 // }
 
 // export default Maps;
